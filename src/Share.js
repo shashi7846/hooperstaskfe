77859e3346d8ec832a1c107a1ece8938047c008c
shashi7846 <shashikanth7846@gmail.com>
2021-06-18 21:17:38 +0530

    first commit

--- src/Share.js
+++ src/Share.js
@@ -13,26 +13,62 @@ import {
    
     WhatsappIcon
   } from "react-share";
+  import { FacebookShareButton,WhatsappShareButton,TelegramShareButton,TwitterShareButton,PinterestShareButton,EmailShareButton,
+RedditShareButton,LinkedinShareButton } from "react-share"
+ 
   import React from 'react';
   import "./Share.css";
+import { useEffect, useState } from "react";
+import {GetColleges} from "./axios";
   
   function Share() {
+
       const shareUrl="https://www.instagram.com/";
+
+
+      let [tabledata,setTabledata]=useState([]);
+
+      useEffect(async () => {
+        let data=await GetColleges()
+      // console.log(data.data)
+      setTabledata(data.data[0])
+      console.log(tabledata[0])
+      
+    },[])
+    
+
+
       return (
           <div className="icon text-center">
-               <WhatsappIcon size={32} round={true} size={32} round={true} />
-               <TelegramIcon size={32} round={true} size={32} round={true} />
-               <TwitterIcon url={shareUrl} size={32} round={true} /> 
-               <FacebookIcon size={32} round={true} size={32} round={true} />
-               <PinterestIcon size={32} round={true} size={32} round={true} />
-               <EmailIcon size={32} round={true} size={32} round={true} />
-               <RedditIcon size={32} round={true} size={32} round={true} />
-               <LinkedinIcon size={32} round={true} size={32} round={true} />
-               
-               
-             
-               
-         
+                   <WhatsappShareButton url={`collegename:${tabledata.collegename}\n collegecode:${tabledata.collegecode}\n collegecountrystate${tabledata.collegecountrystate}\n collegecity${tabledata.collegecity}`}>           
+      <WhatsappIcon size={32} round={true} url="" logoFillColor="white"/>
+  </WhatsappShareButton>
+
+  <EmailShareButton url={`collegename:${tabledata.collegename}\n collegecode:${tabledata.collegecode}\n collegecountrystate${tabledata.collegecountrystate}\n collegecity${tabledata.collegecity}`}>
+      <EmailIcon size={32} round={true} url="" logoFillColor="white"/>
+  </EmailShareButton>
+{/* 
+               <FacebookShareButton url={`collegename:${tabledata.collegename}\n collegecode:${tabledata.collegecode}\n collegecountrystate${tabledata.collegecountrystate}\n collegecity${tabledata.collegecity}`}>
+
+      <FacebookIcon size={32} round={true} url="" logoFillColor="white"/>
+  </FacebookShareButton> */}
+  <TelegramShareButton url={`collegename:${tabledata.collegename}\n collegecode:${tabledata.collegecode}\n collegecountrystate${tabledata.collegecountrystate}\n collegecity${tabledata.collegecity}`}>
+      <TelegramIcon size={32} round={true} url="www.telegram.com" logoFillColor="white"/>
+  </TelegramShareButton>
+  
+  <TwitterShareButton url={`collegename:${tabledata.collegename}\n collegecode:${tabledata.collegecode}\n collegecountrystate${tabledata.collegecountrystate}\n collegecity${tabledata.collegecity}`}>
+      <TwitterIcon size={32} round={true} url="" logoFillColor="white"/>
+  </TwitterShareButton>
+
+  <LinkedinShareButton url={`collegename:${tabledata.collegename}\n collegecode:${tabledata.collegecode}\n collegecountrystate${tabledata.collegecountrystate}\n collegecity${tabledata.collegecity}`}>
+      <LinkedinIcon size={32} round={true} url="" logoFillColor="white"/>
+  </LinkedinShareButton>
+
+ 
+  
+  <PinterestShareButton url={`collegename:${tabledata.collegename}\n collegecode:${tabledata.collegecode}\n collegecountrystate${tabledata.collegecountrystate}\n collegecity${tabledata.collegecity}`}>
+      <PinterestIcon size={32} round={true} url="" logoFillColor="white"/>
+  </PinterestShareButton>
           
           </div>
       )
